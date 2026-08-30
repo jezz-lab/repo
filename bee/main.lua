33d781f09c93044723eb8c01d2f596e06a895759
jezz-lab <jezzby408@gmail.com>
2026-08-30 10:34:47 +0800

    Update main.lua

--- bee/main.lua
+++ bee/main.lua
@@ -14,7 +14,7 @@ local playerGui = player:WaitForChild("PlayerGui")
 --==================================================
 
 local BASE_URL =
-	"https://raw.githubusercontent.com/jezz-lab/rep/main/bee/"
+	"https://raw.githubusercontent.com/jezz-lab/repo/main/bee/"
 
 local ERROR_PANEL_URL = BASE_URL .. "ErrorPanel.lua"
 
